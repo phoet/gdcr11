9d726910bda06c54c405dd6c9dd0e1ba3fa2864d
Peter Schröder <phoetmail@googlemail.com>
2011-12-03 10:03:03 +0100

    level_1

--- lib/life.rb
+++ lib/life.rb
@@ -1,2 +1,28 @@
 class Life
 end
+
+class World
+  attr_accessor :map
+  
+  def initialize(width, height)
+    @map = Array.new(height) {|i| Array.new(width) } 
+  end
+  
+  def cell_size
+    self.to_s.count("O")
+  end
+  
+  def to_s
+    @map.map { |x| x.map { |y| y.nil? ? 'X' : 'O' }.join }.join("\n")
+  end
+end
+
+class Cell
+  def initialize
+    @status = :alive
+  end
+  
+  def alive?
+    @status == :alive
+  end
+end
--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -1,7 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/life')
 
-describe Life do
-  it "should do something" do
-    fail
+EXAMPLE_WORLD = 
+"XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX"
+
+
+# describe Life do
+#   it "should do something" do
+#     fail
+#   end
+# end
+
+describe World do
+  
+  let(:world) { World.new 100, 120 }
+  let(:small_world) { World.new 10, 4 }
+  
+  it "should initialize" do
+    world.map.should have(120).elements
+    world.map.first.should have(100).elements
   end
+  
+  it "should have a string representation" do
+    small_world.to_s.should eql(EXAMPLE_WORLD)
+  end
+  
+  it "should tell me no of cells alive" do
+    world.cell_size.should eql(0)
+  end
+  
+end
+
+describe Cell do
+  
+  let(:cell) { Cell.new }
+  
+  it "should initialize" do
+    cell.alive?.should be_true
+  end
+  
 end
